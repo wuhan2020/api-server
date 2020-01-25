43a7c947225b83c6f7aba9639ed6f5c00bef93ef
LiuChangFreeman <liuchangfreeman@live.com>
2020-01-26 01:11:42 +0800

    little fixes (#6)
    * add gitignore
    
    * add hospitals
    
    * adapt for serverless
    
    * inline import

--- utils.py
+++ utils.py
@@ -8,21 +8,19 @@ import csv
 import os 
 
 data = Blueprint('register', __name__)
-from index import *
 PATH_HOSPITAL = os.path.join(path_home, "HOSPITAL.csv")
 PATH_LOGISTICAL = os.path.join(path_home, "LOGISTICAL.csv")
 
 if platform.system()=="Linux":
     path_home="/home/wuhan2020/wuhan2020"
 else:
+    from index import app
     path_home=os.path.join(app.root_path,"wuhan2020")
 # wuhan2020文件夹为https://github.com/wuhan2020/wuhan2020项目文件的本地clone
 # 阿里云serverless使用挂载nas远程目录来存放缓存文件；在本机调试时，缓存文件夹将存放在项目根目录
 
 if not os.path.exists(path_home):
     os.mkdir(path_home)
-PATH_HOSPITAL=os.path.join(path_home,"HOSPITAL.csv")
-
 
 
 @data.route('/logistical_list')
