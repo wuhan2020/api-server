4d68b98cc9b506d0696f4f44d52ad38e35329a11
Ke Wu <ke.wu@airbnb.com>
2020-01-27 13:14:59 -0800

    [文档][端口] 更正文档说明使用5000端口

--- index.py
+++ index.py
@@ -7,11 +7,12 @@ app.debug = True
 path_prefix= "/wuhan2020"
 # url请求前缀，在http路径中要加/wuhan2020/
 app.register_blueprint(data, url_prefix=path_prefix)
-#使用flask蓝图功能来注册http-router
+# 使用flask蓝图功能来注册http-router
 
 
 def handler(environ, start_response):
     return app(environ, start_response)
 
 if __name__ == '__main__':
+    # 使用默认端口5000
     app.run()
